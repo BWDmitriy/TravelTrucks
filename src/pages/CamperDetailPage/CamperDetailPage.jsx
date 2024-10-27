0b290f69bc28a68a5a97eeb9b7afc4a4f5c33204
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-10-27 19:21:52 +0200

    CamperDetailPage WiP

--- src/pages/CamperDetailPage/CamperDetailPage.jsx
+++ src/pages/CamperDetailPage/CamperDetailPage.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCampers } from '../../features/campers/campersSlice';
 import './CamperDetailPage.css';
+import sprite from '../../assets/symbol-defs.svg'
 
 function CamperDetailPage() {
   const { id } = useParams();
@@ -23,9 +24,25 @@ function CamperDetailPage() {
 
   return (
     <div className="camper-detail">
-      <h1>{camper.name}</h1>
-      <p>Price: ${camper.price.toFixed(2)}</p>
-      {/* Add more details and booking form here */}
+      <h1>{camper.name}</h1><p><svg className="icon">
+                <use xlinkHref={`${sprite}#icon-star-pressed`} ></use>
+                </svg> {camper.rating}({camper.reviews.length} Reviews) <svg className="icon">
+              <use xlinkHref={`${sprite}#icon-map`}></use>
+                  </svg>{camper.location}</p>
+      <p>Price: Є{camper.price.toFixed(2)}</p>
+      
+
+      <ul className="camper-features">
+        {(camper.features || []).map((feature, index) => (
+          <li key={index}>{feature}</li>
+        ))}
+      </ul>
+      <div className="camper-gallery">
+        {(camper.gallery || []).map((image, index) => (
+          <img key={index} src={image.thumb} alt={`Gallery ${index + 1}`} className="gallery-image" />
+        ))}
+      </div>
+            <p>{camper.description}</p>
     </div>
   );
 }
